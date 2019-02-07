fd5cc8439372144e284cdadc7ae58f666a865a68
Manuel Ariza <mga@manuels-mbp.home>
2019-02-07 22:23:58 +0000

    commit

--- js/index.js
+++ js/index.js
@@ -87,7 +87,7 @@ const songList = [
         wide: 'https://i.ytimg.com/vi/boeuk8N_Gsw/maxresdefault.jpg',
       },
     },
-    url: `${archiveBase}/LostOnYou_201610/Lost On You.mp3`,
+    url: `/mp3/Absentia.mp3`,
   },
   {
     title: 'Bad Liar',
